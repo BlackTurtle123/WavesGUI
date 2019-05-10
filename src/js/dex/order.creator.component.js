fb206a5733c85359029f20308e51386e67429b82
bnagtega <bram.nagtegals@trendminer.com>
2019-05-10 10:58:31 +0200

    make fees changeable

--- src/js/dex/order.creator.component.js
+++ src/js/dex/order.creator.component.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var FEE = 0.04,
+    var FEE = '0.04',
         BALANCE_UPDATE_DELAY = 5000;
 
     function OrderCreatorController($interval, applicationContext, matcherApiService) {
@@ -81,6 +81,10 @@
                 ctrl.sell.amount = balance;
                 ctrl.updateSellTotal();
             }
+        };
+
+        ctrl.updateFee = function() {
+            FEE = ctrl.sell.fee;
         };
 
         intervalPromise = $interval(refreshBalances, BALANCE_UPDATE_DELAY);
